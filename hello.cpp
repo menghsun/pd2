2963531af06950f370adb3ca49792f4d03658c73
menghsun <tsaimh@gmail.com>
2015-02-27 21:26:49 +0800

    add three files

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>    // for using cout
+using namespace std;   // avoid repeating ¡§std::¡¨
+int main()
+{
+	std::cout << "Hello ";
+	cout << "NCKU " << "CSIE 107!!" << endl;
+	return 0;
+}
---
+++ including_file.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+#include "included_file.h"
+	std::cout << "including_file!\n";
+	return 0;
+}
