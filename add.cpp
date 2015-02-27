0d088564e16c6ea438a7b1c7dd9bfdd020816777
menghsun <tsaimh@gmail.com>
2015-02-27 22:39:39 +0800

    add file

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num1, num2;
+	cout << "Please enter the first number: ";
+	cin >> num1;
+	cout << "Please enter the second number: ";
+	cin >> num2;
+	cout << "Sum of the two numbers are: " << num1 + num2 << endl;
+	return 0;
+}
